3507b90b35f28ebe2775ac8fc7f7f0ad1f67c3bd
damonzaza <d.zamosenchuk@gmail.com>
2016-11-01 17:28:32 +0200

    Save first task

--- src/Task1.java
+++ src/Task1.java
@@ -4,8 +4,27 @@
  * чи є введене число парним, якщо воно парне вивести на екран even, інакше - odd.
  * Created by Dmytro Zamosenchuk on 01.11.16.
  */
+import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
+        int a=0;
+        int Result;
 
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("Input positive integer: ");
+            a = in.nextInt();
+            if (a < 0) {
+                System.out.println("Integer is negative!");
+            }
+        }
+        while (a < 0);
+        Result=a%2;
+        if (Result == 0) {
+            System.out.print("Integer is odd.");
+        }
+        else {
+            System.out.print("Integer is even.");
+        }
     }
 }
