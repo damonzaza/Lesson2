6b7305285f384cca0cf258a28be7d76a9124e0ae
damonzaza <d.zamosenchuk@gmail.com>
2016-11-02 21:24:19 +0200

    Third task

--- src/Task3.java
+++ src/Task3.java
@@ -1,12 +1,18 @@
 /**
  * Написати програму, яка буде виводити числа, починаючи від 10, закінчуючи 0 з інтервалом в -1,
- * числа виводити через  * знак коми : ",". Якщо буде зайва кома після останнього числа - нічого
+ * числа виводити через  знак коми : ",". Якщо буде зайва кома після останнього числа - нічого
  * страшного.
- * Created by dzamosenchuk on 01.11.16.
+ * Created by Dmytro Zamosenchuk on 01.11.16.
  */
 
 public class Task3 {
     public static void main(String[] args) {
-
+        System.out.println("Numbers from 10 to zero")
+        for (int i = 10; i >= 0; i--) {
+            System.out.print (" " + i);
+            if (i > 0) {
+                System.out.println (",");
+            }
+        }
     }
 }
