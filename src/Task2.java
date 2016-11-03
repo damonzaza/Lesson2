f4e2749d3e8874e2d393b9c7b314ec0b90051745
damonzaza <d.zamosenchuk@gmail.com>
2016-11-03 18:16:59 +0200

    Save 8th task

--- src/Task2.java
+++ src/Task2.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         int a=0;
-       // String Phrase;
         Scanner in = new Scanner(System.in);
         do {
             System.out.print("Input positive integer: ");
---
+++ src/Task8.java
@@ -0,0 +1,34 @@
+/**
+ * Написати програму, яка виводить таблицю множення, проте замість чисел,
+ * які лежать вище головної діагоналі (1, 4, 9, 16, 25, ..., 81), виводяться решітки.
+ * Created by Dmytro Zamosenchuk on 03.11.16.
+ */
+public class Task8 {
+    public static void main(String[] args) {
+        String Result, Result2;
+
+        System.out.println ("Multiplication table");
+        System.out.println (" | 1  2  3  4  5  6  7  8  9");
+        System.out.println ("_____________________________");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i +"| ");
+            for (int j = 1; j <= 9; j++) {
+                Result= String.valueOf(i*j);
+                if (i > j ) {
+                    Result2="#";
+                }
+                else {
+                    Result2=Result;
+                }
+                if ( Result >= 10 ){
+                    System.out.print(Result + " ");
+                }
+                else {
+                    System.out.print(Result + "  ");
+                }
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
